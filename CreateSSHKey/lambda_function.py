422954d45e10fbcb3a3e93c478999aedf449e8a5
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2020-03-12 21:41:03 +0100

    python black formatting

--- CreateSSHKey/lambda_function.py
+++ CreateSSHKey/lambda_function.py
@@ -16,65 +16,79 @@ import json
 
 def lambda_handler(event, context):
     try:
-        if event['RequestType'] == 'Create':
+        if event["RequestType"] == "Create":
             # Generate keys
-            new_key = rsa.generate_private_key(backend=crypto_default_backend(), public_exponent=65537, key_size=2048)
+            new_key = rsa.generate_private_key(
+                backend=crypto_default_backend(), public_exponent=65537, key_size=2048
+            )
             priv_key = new_key.private_bytes(
                 crypto_serialization.Encoding.PEM,
                 crypto_serialization.PrivateFormat.PKCS8,
-                crypto_serialization.NoEncryption()
+                crypto_serialization.NoEncryption(),
             )
             pub_key = new_key.public_key().public_bytes(
                 crypto_serialization.Encoding.OpenSSH,
-                crypto_serialization.PublicFormat.OpenSSH
+                crypto_serialization.PublicFormat.OpenSSH,
             )
             print(priv_key)
             print(pub_key)
             print(event)
             # Encrypt private key
-            kms = boto3.client('kms', region_name=event["ResourceProperties"]["Region"])
-            enc_key = kms.encrypt(KeyId=event["ResourceProperties"]["KMSKey"], Plaintext=priv_key)['CiphertextBlob']
-            f = open('/tmp/enc_key', 'wb')
+            kms = boto3.client("kms", region_name=event["ResourceProperties"]["Region"])
+            enc_key = kms.encrypt(
+                KeyId=event["ResourceProperties"]["KMSKey"], Plaintext=priv_key
+            )["CiphertextBlob"]
+            f = open("/tmp/enc_key", "wb")
             f.write(enc_key)
             f.close()
             # Upload priivate key to S3
-            s3 = boto3.client('s3')
-            s3.upload_file('/tmp/enc_key', event["ResourceProperties"]["KeyBucket"], 'enc_key')
+            s3 = boto3.client("s3")
+            s3.upload_file(
+                "/tmp/enc_key", event["ResourceProperties"]["KeyBucket"], "enc_key"
+            )
         else:
-            pub_key = event['PhysicalResourceId']
+            pub_key = event["PhysicalResourceId"]
         send(event, context, SUCCESS, {}, pub_key)
     except Exception:
         traceback.print_exc()
-        send(event, context, FAILED, {}, '')
+        send(event, context, FAILED, {}, "")
 
 
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
 
-def send(event, context, response_status, response_data, physical_resource_id=None, no_echo=False):
-    response_url = event['ResponseURL']
+def send(
+    event,
+    context,
+    response_status,
+    response_data,
+    physical_resource_id=None,
+    no_echo=False,
+):
+    response_url = event["ResponseURL"]
 
     print(response_url)
 
-    response_body = {'Status': response_status,
-                     'Reason': 'See the details in CloudWatch Log Stream: ' + context.log_stream_name,
-                     'PhysicalResourceId': physical_resource_id or context.log_stream_name, 'StackId': event['StackId'],
-                     'RequestId': event['RequestId'], 'NoEcho': no_echo, 'Data': response_data}
+    response_body = {
+        "Status": response_status,
+        "Reason": "See the details in CloudWatch Log Stream: "
+        + context.log_stream_name,
+        "PhysicalResourceId": physical_resource_id or context.log_stream_name,
+        "StackId": event["StackId"],
+        "RequestId": event["RequestId"],
+        "NoEcho": no_echo,
+        "Data": response_data,
+    }
 
     json_response_body = json.dumps(response_body)
 
     print("Response body:\n" + json_response_body)
 
-    headers = {
-        'content-type': '',
-        'content-length': str(len(json_response_body))
-    }
+    headers = {"content-type": "", "content-length": str(len(json_response_body))}
 
     try:
-        response = requests.put(response_url,
-                                data=json_response_body,
-                                headers=headers)
+        response = requests.put(response_url, data=json_response_body, headers=headers)
         print("Status code: " + response.reason)
     except Exception as e:
         print("send(..) failed executing requests.put(..): " + str(e))
--- DeleteBucketContents/lambda_function.py
+++ DeleteBucketContents/lambda_function.py
@@ -13,72 +13,96 @@ import json
 
 def lambda_handler(event, context):
     try:
-        if event['RequestType'] == 'Delete':
-            s3 = boto3.client('s3')
+        if event["RequestType"] == "Delete":
+            s3 = boto3.client("s3")
             # Delete KeyBucket contents
             print()
-            'Getting KeyBucket objects...'
-            s3objects = s3.list_objects_v2(Bucket=event["ResourceProperties"]["KeyBucket"])
-            if 'Contents' in list(s3objects.keys()):
+            "Getting KeyBucket objects..."
+            s3objects = s3.list_objects_v2(
+                Bucket=event["ResourceProperties"]["KeyBucket"]
+            )
+            if "Contents" in list(s3objects.keys()):
                 print()
-                'Deleting KeyBucket objects %s...' % str([{'Key': key['Key']} for key in s3objects['Contents']])
-                s3.delete_objects(Bucket=event["ResourceProperties"]["KeyBucket"],
-                                  Delete={'Objects': [{'Key': key['Key']} for key in s3objects['Contents']]})
+                "Deleting KeyBucket objects %s..." % str(
+                    [{"Key": key["Key"]} for key in s3objects["Contents"]]
+                )
+                s3.delete_objects(
+                    Bucket=event["ResourceProperties"]["KeyBucket"],
+                    Delete={
+                        "Objects": [
+                            {"Key": key["Key"]} for key in s3objects["Contents"]
+                        ]
+                    },
+                )
             # Delete Output bucket contents and versions
             print()
-            'Getting OutputBucket objects...'
+            "Getting OutputBucket objects..."
             objects = []
-            versions = s3.list_object_versions(Bucket=event["ResourceProperties"]["OutputBucket"])
+            versions = s3.list_object_versions(
+                Bucket=event["ResourceProperties"]["OutputBucket"]
+            )
             while versions:
-                if 'Versions' in list(versions.keys()):
-                    for v in versions['Versions']:
-                        objects.append({'Key': v['Key'], 'VersionId': v['VersionId']})
-                if 'DeleteMarkers' in list(versions.keys()):
-                    for v in versions['DeleteMarkers']:
-                        objects.append({'Key': v['Key'], 'VersionId': v['VersionId']})
-                if versions['IsTruncated']:
-                    versions = s3.list_object_versions(Bucket=event["ResourceProperties"]["OutputBucket"],
-                                                       VersionIdMarker=versions['NextVersionIdMarker'])
+                if "Versions" in list(versions.keys()):
+                    for v in versions["Versions"]:
+                        objects.append({"Key": v["Key"], "VersionId": v["VersionId"]})
+                if "DeleteMarkers" in list(versions.keys()):
+                    for v in versions["DeleteMarkers"]:
+                        objects.append({"Key": v["Key"], "VersionId": v["VersionId"]})
+                if versions["IsTruncated"]:
+                    versions = s3.list_object_versions(
+                        Bucket=event["ResourceProperties"]["OutputBucket"],
+                        VersionIdMarker=versions["NextVersionIdMarker"],
+                    )
                 else:
                     versions = False
             if objects:
-                s3.delete_objects(Bucket=event["ResourceProperties"]["OutputBucket"], Delete={'Objects': objects})
-        send(event, context, SUCCESS, {}, '')
+                s3.delete_objects(
+                    Bucket=event["ResourceProperties"]["OutputBucket"],
+                    Delete={"Objects": objects},
+                )
+        send(event, context, SUCCESS, {}, "")
     except Exception:
         print()
         traceback.print_exc()
-        send(event, context, FAILED, {}, '')
+        send(event, context, FAILED, {}, "")
 
 
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
 
-def send(event, context, response_status, response_data, physical_resource_id=None, no_echo=False):
-    response_url = event['ResponseURL']
+def send(
+    event,
+    context,
+    response_status,
+    response_data,
+    physical_resource_id=None,
+    no_echo=False,
+):
+    response_url = event["ResponseURL"]
 
     print(response_url)
 
-    response_body = {'Status': response_status,
-                     'Reason': 'See the details in CloudWatch Log Stream: ' + context.log_stream_name,
-                     'PhysicalResourceId': physical_resource_id or context.log_stream_name, 'StackId': event['StackId'],
-                     'RequestId': event['RequestId'], 'LogicalResourceId': event['LogicalResourceId'],
-                     'NoEcho': no_echo,
-                     'Data': response_data}
+    response_body = {
+        "Status": response_status,
+        "Reason": "See the details in CloudWatch Log Stream: "
+        + context.log_stream_name,
+        "PhysicalResourceId": physical_resource_id or context.log_stream_name,
+        "StackId": event["StackId"],
+        "RequestId": event["RequestId"],
+        "LogicalResourceId": event["LogicalResourceId"],
+        "NoEcho": no_echo,
+        "Data": response_data,
+    }
 
     json_response_body = json.dumps(response_body)
 
     print("Response body:\n" + json_response_body)
 
-    headers = {
-        'content-type': '',
-        'content-length': str(len(json_response_body))
-    }
+    headers = {"content-type": "", "content-length": str(len(json_response_body))}
 
     try:
-        response = requests.put(response_url,
-                                data=json_response_body,
-                                headers=headers)
+        response = requests.put(response_url, data=json_response_body, headers=headers)
         print("Status code: " + response.reason)
     except Exception as e:
         print("send(..) failed executing requests.put(..): " + str(e))
