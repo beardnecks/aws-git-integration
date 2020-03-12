359d48146656fcee13f756f8e3e5e39e5288ec41
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2020-03-12 13:46:50 +0100

    Added send function from cfnresponse. Fixes outdated dependency issues

--- CreateSSHKey/lambda_function.py
+++ CreateSSHKey/lambda_function.py
@@ -5,12 +5,13 @@
 #  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 #  See the License for the specific language governing permissions and limitations under the License.
 
-import cfnresponse
 import traceback
 import boto3
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
+import requests
+import json
 
 
 def lambda_handler(event, context):
@@ -41,7 +42,43 @@ def lambda_handler(event, context):
             s3.upload_file('/tmp/enc_key', event["ResourceProperties"]["KeyBucket"], 'enc_key')
         else:
             pub_key = event['PhysicalResourceId']
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {}, pub_key)
+        send(event, context, SUCCESS, {}, pub_key)
     except:
         traceback.print_exc()
-        cfnresponse.send(event, context, cfnresponse.FAILED, {}, '')
+        send(event, context, FAILED, {}, '')
+
+
+SUCCESS = "SUCCESS"
+FAILED = "FAILED"
+
+
+def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
+    responseUrl = event['ResponseURL']
+
+    print(responseUrl)
+
+    responseBody = {}
+    responseBody['Status'] = responseStatus
+    responseBody['Reason'] = 'See the details in CloudWatch Log Stream: ' + context.log_stream_name
+    responseBody['PhysicalResourceId'] = physicalResourceId or context.log_stream_name
+    responseBody['StackId'] = event['StackId']
+    responseBody['RequestId'] = event['RequestId']
+    responseBody['NoEcho'] = noEcho
+    responseBody['Data'] = responseData
+
+    json_responseBody = json.dumps(responseBody)
+
+    print("Response body:\n" + json_responseBody)
+
+    headers = {
+        'content-type': '',
+        'content-length': str(len(json_responseBody))
+    }
+
+    try:
+        response = requests.put(responseUrl,
+                                data=json_responseBody,
+                                headers=headers)
+        print("Status code: " + response.reason)
+    except Exception as e:
+        print("send(..) failed executing requests.put(..): " + str(e))
--- DeleteBucketContents/lambda_function.py
+++ DeleteBucketContents/lambda_function.py
@@ -5,10 +5,10 @@
 #  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 #  See the License for the specific language governing permissions and limitations under the License.
 
-import cfnresponse
 import traceback
 import boto3
-
+import requests
+import json
 
 def lambda_handler(event, context):
     try:
@@ -42,8 +42,45 @@ def lambda_handler(event, context):
                     versions = False
             if objects != []:
                 s3.delete_objects(Bucket=event["ResourceProperties"]["OutputBucket"], Delete={'Objects': objects})
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {}, '')
+        send(event, context, SUCCESS, {}, '')
     except:
         print()
         traceback.print_exc()
-        cfnresponse.send(event, context, cfnresponse.FAILED, {}, '')
+        send(event, context, FAILED, {}, '')
+
+
+SUCCESS = "SUCCESS"
+FAILED = "FAILED"
+
+
+def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
+    responseUrl = event['ResponseURL']
+
+    print(responseUrl)
+
+    responseBody = {}
+    responseBody['Status'] = responseStatus
+    responseBody['Reason'] = 'See the details in CloudWatch Log Stream: ' + context.log_stream_name
+    responseBody['PhysicalResourceId'] = physicalResourceId or context.log_stream_name
+    responseBody['StackId'] = event['StackId']
+    responseBody['RequestId'] = event['RequestId']
+    responseBody['LogicalResourceId'] = event['LogicalResourceId']
+    responseBody['NoEcho'] = noEcho
+    responseBody['Data'] = responseData
+
+    json_responseBody = json.dumps(responseBody)
+
+    print("Response body:\n" + json_responseBody)
+
+    headers = {
+        'content-type': '',
+        'content-length': str(len(json_responseBody))
+    }
+
+    try:
+        response = requests.put(responseUrl,
+                                data=json_responseBody,
+                                headers=headers)
+        print("Status code: " + response.reason)
+    except Exception as e:
+        print("send(..) failed executing requests.put(..): " + str(e))
