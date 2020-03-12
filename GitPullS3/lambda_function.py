f65a87c1285a8f1668811dab4fece01980c3a9ce
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2020-03-12 21:38:59 +0100

    python black formating

--- GitPullS3/lambda_function.py
+++ GitPullS3/lambda_function.py
@@ -10,6 +10,7 @@ import hashlib
 import hmac
 import logging
 import os
+
 # Regex
 import re
 import shutil
@@ -21,26 +22,28 @@ from boto3 import client
 from git import Remote, Repo, exc
 
 # If true the function will not include .git folder in the zip
-exclude_git = bool(distutils.util.strtobool(os.environ['ExcludeGit']))
+exclude_git = bool(distutils.util.strtobool(os.environ["ExcludeGit"]))
 
-key = 'enc_key'
+key = "enc_key"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-logger.handlers[0].setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s'))
-logging.getLogger('boto3').setLevel(logging.ERROR)
-logging.getLogger('botocore').setLevel(logging.ERROR)
+logger.handlers[0].setFormatter(
+    logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
+)
+logging.getLogger("boto3").setLevel(logging.ERROR)
+logging.getLogger("botocore").setLevel(logging.ERROR)
 
-s3 = client('s3')
-kms = client('kms')
+s3 = client("s3")
+kms = client("kms")
 
 
 def write_key(filename, contents):
-    logger.info('Writing keys to /tmp/...')
+    logger.info("Writing keys to /tmp/...")
     mode = stat.S_IRUSR | stat.S_IWUSR
     umask_original = os.umask(0)
     try:
-        handle = os.fdopen(os.open(filename, os.O_WRONLY | os.O_CREAT, mode), 'wb')
+        handle = os.fdopen(os.open(filename, os.O_WRONLY | os.O_CREAT, mode), "wb")
     finally:
         os.umask(umask_original)
 
@@ -49,22 +52,26 @@ def write_key(filename, contents):
 
 
 def get_keys(keybucket, pubkey, update=False):
-    if not os.path.isfile('/tmp/id_rsa') or not os.path.isfile('/tmp/id_rsa.pub') or update:
-        logger.info('Keys not found on Lambda container, fetching from S3...')
-        enckey = s3.get_object(Bucket=keybucket, Key=key)['Body'].read()
-        privkey = kms.decrypt(CiphertextBlob=enckey)['Plaintext']
-        write_key('/tmp/id_rsa', privkey)
-        write_key('/tmp/id_rsa.pub', str.encode(pubkey))
+    if (
+        not os.path.isfile("/tmp/id_rsa")
+        or not os.path.isfile("/tmp/id_rsa.pub")
+        or update
+    ):
+        logger.info("Keys not found on Lambda container, fetching from S3...")
+        enckey = s3.get_object(Bucket=keybucket, Key=key)["Body"].read()
+        privkey = kms.decrypt(CiphertextBlob=enckey)["Plaintext"]
+        write_key("/tmp/id_rsa", privkey)
+        write_key("/tmp/id_rsa.pub", str.encode(pubkey))
 
 
 def init_remote(repo, name, url):
-    remote = repo.remotes.create(name, url, '+refs/*:refs/*')
+    remote = repo.remotes.create(name, url, "+refs/*:refs/*")
     return remote
 
 
 def create_repo(repo_path, remote_url):
     if os.path.exists(repo_path):
-        logger.info('Cleaning up repo path...')
+        logger.info("Cleaning up repo path...")
         shutil.rmtree(repo_path)
     repo = Repo.clone_from(remote_url, repo_path, depth=1)
 
@@ -78,13 +85,15 @@ def pull_repo(repo, branch_name, remote_url):
             remote_exists = True
             remote = r
     if not remote_exists:
-        remote = repo.create_remote('origin', remote_url)
-    logger.info('Fetching and merging changes from %s branch %s', remote_url, branch_name)
+        remote = repo.create_remote("origin", remote_url)
+    logger.info(
+        "Fetching and merging changes from %s branch %s", remote_url, branch_name
+    )
     remote.fetch()
-    if branch_name.startswith('tags/'):
-        ref = 'refs/' + branch_name
+    if branch_name.startswith("tags/"):
+        ref = "refs/" + branch_name
     else:
-        ref = 'refs/remotes/origin/' + branch_name
+        ref = "refs/remotes/origin/" + branch_name
     remote_branch_id = repo.lookup_reference(ref).target
     repo.checkout_tree(repo.get(remote_branch_id))
     # branch_ref = repo.lookup_reference('refs/heads/' + branch_name)
@@ -94,191 +103,240 @@ def pull_repo(repo, branch_name, remote_url):
 
 
 def zip_repo(repo_path, repo_name):
-    logger.info('Creating zipfile...')
-    zf = ZipFile('/tmp/' + repo_name.replace('/', '_') + '.zip', 'w')
+    logger.info("Creating zipfile...")
+    zf = ZipFile("/tmp/" + repo_name.replace("/", "_") + ".zip", "w")
     for dirname, subdirs, files in os.walk(repo_path):
         if exclude_git:
             try:
-                subdirs.remove('.git')
+                subdirs.remove(".git")
             except ValueError:
                 pass
-        zdirname = dirname[len(repo_path) + 1:]
+        zdirname = dirname[len(repo_path) + 1 :]
         zf.write(dirname, zdirname)
         for filename in files:
             zf.write(os.path.join(dirname, filename), os.path.join(zdirname, filename))
     zf.close()
-    return '/tmp/' + repo_name.replace('/', '_') + '.zip'
+    return "/tmp/" + repo_name.replace("/", "_") + ".zip"
 
 
 def push_s3(filename, repo_name, prefix, outputbucket):
-    s3key = '%s/%s/%s' % (repo_name, prefix, filename.replace('/tmp/', ''))
-    logger.info('pushing zip to s3://%s/%s' % (outputbucket, s3key))
-    data = open(filename, 'rb')
+    s3key = "%s/%s/%s" % (repo_name, prefix, filename.replace("/tmp/", ""))
+    logger.info("pushing zip to s3://%s/%s" % (outputbucket, s3key))
+    data = open(filename, "rb")
     s3.put_object(Bucket=outputbucket, Body=data, Key=s3key)
-    logger.info('Completed S3 upload...')
+    logger.info("Completed S3 upload...")
 
 
 def lambda_handler(event, context):
     print(event)  # TODO: Remove debug
-    keybucket = event['context']['key-bucket']
-    outputbucket = event['context']['output-bucket']
-    pubkey = event['context']['public-key']
+    keybucket = event["context"]["key-bucket"]
+    outputbucket = event["context"]["output-bucket"]
+    pubkey = event["context"]["public-key"]
     # Source IP ranges to allow requests from,
     # if the IP is in one of these the request will not be chacked for an api key
     ipranges = []
-    for i in event['context']['allowed-ips'].split(','):
-        ipranges.append(ip_network('%s' % i))
+    for i in event["context"]["allowed-ips"].split(","):
+        ipranges.append(ip_network("%s" % i))
     # APIKeys, it is recommended to use a different API key for each repo that uses this function
-    apikeys = event['context']['api-secrets'].split(',')
-    ip = ip_address(event['context']['source-ip'])
+    apikeys = event["context"]["api-secrets"].split(",")
+    ip = ip_address(event["context"]["source-ip"])
     # Check if it is a pull request(PR) or not.
     pr = False
-    # Unsure wether this if statement works or not, must be tested. 
+    # Unsure wether this if statement works or not, must be tested.
     # Figure out how to check if event == pull_request.
-    if event['params']['header']['X-GitHub-Event'] == 'pull_request':
+    if event["params"]["header"]["X-GitHub-Event"] == "pull_request":
         pr = True
     # Check if it is a push or not
     push = False
-    # Unsure wether this if statement works or not, must be tested. 
-    if event['params']['header']['X-GitHub-Event'] == 'push':
+    # Unsure wether this if statement works or not, must be tested.
+    if event["params"]["header"]["X-GitHub-Event"] == "push":
         push = True
 
     secure = False
     for net in ipranges:
         if ip in net:
             secure = True
-    if 'X-Git-Token' in list(event['params']['header'].keys()):
-        if event['params']['header']['X-Git-Token'] in apikeys:
+    if "X-Git-Token" in list(event["params"]["header"].keys()):
+        if event["params"]["header"]["X-Git-Token"] in apikeys:
             secure = True
-    if 'X-Gitlab-Token' in list(event['params']['header'].keys()):
-        if event['params']['header']['X-Gitlab-Token'] in apikeys:
+    if "X-Gitlab-Token" in list(event["params"]["header"].keys()):
+        if event["params"]["header"]["X-Gitlab-Token"] in apikeys:
             secure = True
-    if 'X-Hub-Signature' in list(event['params']['header'].keys()):
+    if "X-Hub-Signature" in list(event["params"]["header"].keys()):
         for k in apikeys:
-            if 'use-sha256' in event['context']:
-                k1 = hmac.new(str(k), str(event['context']['raw-body']), hashlib.sha256).hexdigest()
-                k2 = str(event['params']['header']['X-Hub-Signature'].replace('sha256=', ''))
+            if "use-sha256" in event["context"]:
+                k1 = hmac.new(
+                    str(k), str(event["context"]["raw-body"]), hashlib.sha256
+                ).hexdigest()
+                k2 = str(
+                    event["params"]["header"]["X-Hub-Signature"].replace("sha256=", "")
+                )
             else:
-                k1 = hmac.new(str(k), str(event['context']['raw-body']), hashlib.sha1).hexdigest()
-                k2 = str(event['params']['header']['X-Hub-Signature'].replace('sha1=', ''))
+                k1 = hmac.new(
+                    str(k), str(event["context"]["raw-body"]), hashlib.sha1
+                ).hexdigest()
+                k2 = str(
+                    event["params"]["header"]["X-Hub-Signature"].replace("sha1=", "")
+                )
             if k1 == k2:
                 secure = True
 
     try:
         # GitHub
-        full_name = event['body-json']['repository']['full_name']
+        full_name = event["body-json"]["repository"]["full_name"]
     except KeyError:
         try:
             # BitBucket #14
-            full_name = event['body-json']['repository']['fullName']
+            full_name = event["body-json"]["repository"]["fullName"]
         except KeyError:
             try:
                 # GitLab
-                full_name = event['body-json']['repository']['path_with_namespace']
+                full_name = event["body-json"]["repository"]["path_with_namespace"]
             except KeyError:
                 try:
                     # GitLab 8.5+
-                    full_name = event['body-json']['project']['path_with_namespace']
+                    full_name = event["body-json"]["project"]["path_with_namespace"]
                 except KeyError:
                     try:
                         # BitBucket server
-                        full_name = event['body-json']['repository']['name']
+                        full_name = event["body-json"]["repository"]["name"]
                     except KeyError:
                         # BitBucket pull-request
-                        full_name = event['body-json']['pullRequest']['fromRef']['repository']['name']
+                        full_name = event["body-json"]["pullRequest"]["fromRef"][
+                            "repository"
+                        ]["name"]
     if not secure:
-        logger.error('Source IP %s is not allowed' % event['context']['source-ip'])
-        raise Exception('Source IP %s is not allowed' % event['context']['source-ip'])
+        logger.error("Source IP %s is not allowed" % event["context"]["source-ip"])
+        raise Exception("Source IP %s is not allowed" % event["context"]["source-ip"])
 
     # Check if there is PR or a push
     if not (pr or push):
-        logger.error('This is not a Pull Request or a Push')
-        raise Exception('This is not a Pull Request or a Push')
+        logger.error("This is not a Pull Request or a Push")
+        raise Exception("This is not a Pull Request or a Push")
 
     # Check if: Opened PR
     if pr:
-        if 'action' in event['body-json'] and event['body-json']['action'] != 'opened' and not push:
-            logger.error('PR action is not opened, it is %s' % event['body-json']['action'])
-            raise Exception('PR action is not opened, it is %s' % event['body-json']['action'])
+        if (
+            "action" in event["body-json"]
+            and event["body-json"]["action"] != "opened"
+            and not push
+        ):
+            logger.error(
+                "PR action is not opened, it is %s" % event["body-json"]["action"]
+            )
+            raise Exception(
+                "PR action is not opened, it is %s" % event["body-json"]["action"]
+            )
     # Check if PR to master
     # Regex object, string that starts with master, widlcard after. (master*)
     regex = re.compile("^master")
     if pr:
-        if ('base' in event['body-json']['pull_request'] and not regex.match(
-                event['body-json']['pull_request']['base']['ref'])):
-            logger.error('PR is not to master, it is %s' % event['body-json']['pull_request']['base']['ref'])
-            raise Exception('PR is not to master, it is %s' % event['body-json']['pull_request']['base']['ref'])
+        if "base" in event["body-json"]["pull_request"] and not regex.match(
+            event["body-json"]["pull_request"]["base"]["ref"]
+        ):
+            logger.error(
+                "PR is not to master, it is %s"
+                % event["body-json"]["pull_request"]["base"]["ref"]
+            )
+            raise Exception(
+                "PR is not to master, it is %s"
+                % event["body-json"]["pull_request"]["base"]["ref"]
+            )
         else:
             prefix = "dev"
 
     # Check if: Push to master.
     regex = re.compile("^refs/heads/master$")
     if push:
-        if 'ref' in event['body-json'] and not regex.match(event['body-json']['ref']) and not pr:
-            logger.error('Push is not to master, it is to %s' % event['body-json']['ref'])
-            raise Exception('Push is not to master it is to %s' % event['body-json']['ref'])
+        if (
+            "ref" in event["body-json"]
+            and not regex.match(event["body-json"]["ref"])
+            and not pr
+        ):
+            logger.error(
+                "Push is not to master, it is to %s" % event["body-json"]["ref"]
+            )
+            raise Exception(
+                "Push is not to master it is to %s" % event["body-json"]["ref"]
+            )
         else:
             prefix = "prod"
 
     # GitHub publish event
-    if 'action' in event['body-json'] and event['body-json']['action'] == 'published':
-        branch_name = 'tags/%s' % event['body-json']['release']['tag_name']
-        repo_name = full_name + '/release'
+    if "action" in event["body-json"] and event["body-json"]["action"] == "published":
+        branch_name = "tags/%s" % event["body-json"]["release"]["tag_name"]
+        repo_name = full_name + "/release"
     else:
         repo_name = full_name
         try:
             # branch names should contain [name] only, tag names - "tags/[name]"
-            branch_name = event['body-json']['ref'].replace('refs/heads/', '').replace('refs/tags/', 'tags/')
+            branch_name = (
+                event["body-json"]["ref"]
+                .replace("refs/heads/", "")
+                .replace("refs/tags/", "tags/")
+            )
         except KeyError:
             try:
                 # Bibucket server
-                branch_name = event['body-json']['push']['changes'][0]['new']['name']
+                branch_name = event["body-json"]["push"]["changes"][0]["new"]["name"]
             except Exception:
-                branch_name = 'master'
+                branch_name = "master"
     try:
         # GitLab
-        remote_url = event['body-json']['project']['git_ssh_url']
+        remote_url = event["body-json"]["project"]["git_ssh_url"]
     except Exception:
         try:
-            remote_url = 'git@' \
-                         + event['body-json']['repository']['links']['html']['href'].replace('https://',
-                                                                                             '').replace('/',
-                                                                                                         ':',
-                                                                                                         1) + '.git'
+            remote_url = (
+                "git@"
+                + event["body-json"]["repository"]["links"]["html"]["href"]
+                .replace("https://", "")
+                .replace("/", ":", 1)
+                + ".git"
+            )
         except Exception:
             try:
                 # GitHub
-                remote_url = event['body-json']['repository']['ssh_url']
+                remote_url = event["body-json"]["repository"]["ssh_url"]
             except Exception:
                 # Bitbucket
                 try:
-                    for i, url in enumerate(event['body-json']['repository']['links']['clone']):
-                        if url['name'] == 'ssh':
+                    for i, url in enumerate(
+                        event["body-json"]["repository"]["links"]["clone"]
+                    ):
+                        if url["name"] == "ssh":
                             ssh_index = i
-                    remote_url = event['body-json']['repository']['links']['clone'][ssh_index]['href']
+                    remote_url = event["body-json"]["repository"]["links"]["clone"][
+                        ssh_index
+                    ]["href"]
                 except Exception:
                     # BitBucket pull-request
                     for i, url in enumerate(
-                            event['body-json']['pullRequest']['fromRef']['repository']['links']['clone']):
-                        if url['name'] == 'ssh':
+                        event["body-json"]["pullRequest"]["fromRef"]["repository"][
+                            "links"
+                        ]["clone"]
+                    ):
+                        if url["name"] == "ssh":
                             ssh_index = i
 
-                    remote_url = \
-                        event['body-json']['pullRequest']['fromRef']['repository']['links']['clone'][ssh_index]['href']
-    repo_path = '/tmp/%s' % repo_name
+                    remote_url = event["body-json"]["pullRequest"]["fromRef"][
+                        "repository"
+                    ]["links"]["clone"][ssh_index]["href"]
+    repo_path = "/tmp/%s" % repo_name
     # creds = RemoteCallbacks(credentials=get_keys(keybucket, pubkey), )
     get_keys(keybucket, pubkey)
-    git_ssh_cmd = 'ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no'
+    git_ssh_cmd = "ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no"
     try:
-        logger.info('Cloning repository from %s' % remote_url)
-        Repo.clone_from(remote_url, repo_path, depth=1, env=dict(GIT_SSH_COMMAND=git_ssh_cmd))
+        logger.info("Cloning repository from %s" % remote_url)
+        Repo.clone_from(
+            remote_url, repo_path, depth=1, env=dict(GIT_SSH_COMMAND=git_ssh_cmd)
+        )
     except (exc.NoSuchPathError, exc.InvalidGitRepositoryError) as e:
-        logger.error('Error pulling new repo for %s in %s' % (remote_url, repo_path))
+        logger.error("Error pulling new repo for %s in %s" % (remote_url, repo_path))
     zipfile = zip_repo(repo_path, repo_name)
     push_s3(zipfile, repo_name, prefix, outputbucket)
-    logger.info('Cleanup Lambda container...')
+    logger.info("Cleanup Lambda container...")
     shutil.rmtree(repo_path)
     os.remove(zipfile)
-    os.remove('/tmp/id_rsa')
-    os.remove('/tmp/id_rsa.pub')
-    return 'Successfully updated %s' % repo_name
+    os.remove("/tmp/id_rsa")
+    os.remove("/tmp/id_rsa.pub")
+    return "Successfully updated %s" % repo_name
