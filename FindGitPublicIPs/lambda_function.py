bc82bdde468516dd1338f4383a8f0fefcd0753df
Zackeus Bengtsson <zackeusb@stud.ntnu.no>
2020-03-13 20:02:23 +0100

    Use env var for bucketname. Fixed bytes/str error

--- FindGitPublicIPs/lambda_function.py
+++ FindGitPublicIPs/lambda_function.py
@@ -1,41 +1,45 @@
+import os
+
 import boto3
 import requests
 
 
 def lambda_handler(event: dict, context):
+    ip_bucket = os.environ["IPBucket"]
+
     # Bitbucket IPs
     resp = requests.get("https://ip-ranges.atlassian.com")
     bitbucket_resp = resp.json()
 
     valid_ips = ""
     for item in bitbucket_resp["items"]:
-        valid_ips = valid_ips + "," + item
+        valid_ips += item["cidr"] + ","
 
     # Github IPs
     resp = requests.get("https://api.github.com/meta")
     github_resp = resp.json()
 
     for ip in github_resp["hooks"]:
-        valid_ips = valid_ips + "," + ip
+        valid_ips += ip + ","
 
     for ip in github_resp["web"]:
-        valid_ips = valid_ips + "," + ip
+        valid_ips += ip + ","
 
     for ip in github_resp["api"]:
-        valid_ips = valid_ips + "," + ip
+        valid_ips += ip + ","
 
     for ip in github_resp["git"]:
-        valid_ips = valid_ips + "," + ip
+        valid_ips += ip + ","
 
     for ip in github_resp["pages"]:
-        valid_ips = valid_ips + "," + ip
+        valid_ips += ip + ","
 
     for ip in github_resp["importer"]:
-        valid_ips = valid_ips + "," + ip
+        valid_ips += ip + ","
 
     f = open("/tmp/ips", "wb")
-    f.write(valid_ips)
+    f.write(valid_ips.rstrip(",").encode())
     f.close()
     s3 = boto3.client("s3")
-    s3.upload_file("/tmp/ips", event["ResourceProperties"]["IPBucket"], "ips")
+    s3.upload_file("/tmp/ips", ip_bucket, "ips")
     print("Updated valid ips")
